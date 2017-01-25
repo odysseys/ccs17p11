2564c089b2a157da323d704c1513327793a7313c
odysseys <odysseys.tw@gmail.com>
2017-01-25 06:09:56 -0800

    first_trial

--- cc17sp11.py
+++ cc17sp11.py
@@ -1,1 +1,11 @@
 import re
+#print('START')
+#open_file = open( '../pageviews-20161109-000000_short', 'r', encoding = 'utf-8')
+open_file = open( '../test_short', 'r', encoding = 'utf-8')
+#output = open_file.read()
+#print('OPEN FINISH')
+for line in open_file.readlines():
+    print(line, end='')
+    
+open_file.close()
+
